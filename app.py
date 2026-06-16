4f7cdca6e14a30965709906e9ea4b78dd7afd5ba
Iamkeshhh <iamkesh2601@gmail.com>
2026-06-16 18:36:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -246,3 +246,29 @@ st.markdown("""
 
 ✅ XGBoost + Deep Learning Comparison
 """)
+
+import streamlit as st
+import shap
+import pandas as pd
+from utils import load_data, load_model
+
+st.title("🤖 Explainable AI")
+
+df = load_data()
+model = load_model()
+
+X = df.drop("stress_level",axis=1)
+
+explainer = shap.TreeExplainer(model)
+
+shap_values = explainer.shap_values(X)
+
+st.subheader("Feature Importance")
+
+fig = shap.summary_plot(
+    shap_values,
+    X,
+    show=False
+)
+
+st.pyplot()
